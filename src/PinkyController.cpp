8c284d533dfdb46e3d4797349de77053c1b50883
benjamingalvez23 <benjamingalvez2004@gmail.com>
2026-09-12 00:16:18 -0300

    BT

--- src/PinkyController.cpp
+++ src/PinkyController.cpp
@@ -1,17 +1,158 @@
 #include "PinkyController.h"
 
+#include <vector>
+#include <limits>
 
-PinkyController::PinkyController(std::shared_ptr<Character> character):
-	Controller(character){
+
+
+PinkyInfo* PinkyInfo::info = nullptr;
+
+
+
+PinkyController::PinkyController(
+    std::shared_ptr<Character> character
+)
+    : Controller(character),
+      root(std::make_shared<Selector>())
+{
+    auto filter = std::make_shared<Filter>();
+	filter->addCondition(std::make_shared<Powerpill>());
+	filter->addAction(std::make_shared<PinkyFrihtend>()); 		
+	root->addChild(filter);
+
+    auto time = std::make_shared<Filter>();
+    time->addCondition(std::make_shared<TimeOut>());
+	time->addAction(std::make_shared<PinkyScatter>()); 		
+	root->addChild(time);
+    
+    root->addChild(std::make_shared<PinkyChase>());
 }
 
-PinkyController::~PinkyController() {
+PinkyController::~PinkyController()
+{
+}
+
+
+Move PinkyController::getMove(
+    const GameState& game
+)
+{
+
+    PinkyInfo::getInfo()->in_character =
+        character;
+
+
+    PinkyInfo::getInfo()->in_gamestate =
+        &game;
+
+
+
+    root->tick();
+
+
+    return PinkyInfo::getInfo()->out_move;
+}
+
+
+
+Status PinkyChase::update()
+{
+    auto character = PinkyInfo::getInfo()->in_character;
+    auto gs = PinkyInfo::getInfo()->in_gamestate;
+
+    auto pacmanPos = gs->getPacmanPos();
+
+    Move bestMove = PASS;
+
+    float bestDifference = std::numeric_limits<float>::max();
+
+    std::vector<Move> moves;
+
+    if(character->getDirection() == PASS) {
+        moves = gs->getMaze().getPossibleMoves(character->getPos());
+    } 
+    else {
+        moves = gs->getMaze().getGhostLegalMoves(
+            character->getPos(),
+            character->getDirection()
+        );
+    }
+
+    for(auto move : moves)
+    {
+        if(move == PASS) {
+            break;
+        }
+
+        auto nextPos = gs->getMaze().getNeighbour(
+            character->getPos(),
+            move
+        );
+
+        auto nextNode = gs->getMaze().getNodePos(nextPos);
+        auto pacmanNode = gs->getMaze().getNodePos(pacmanPos);
+
+        float dist = euclid2(pacmanNode, nextNode);
+
+        float difference = std::abs(dist - 4.0f);
+
+        if(difference < bestDifference)
+        {
+            bestDifference = difference;
+            bestMove = move;
+        }
+    }
+
+    PinkyInfo::getInfo()->out_move = bestMove;
+
+    return BH_SUCCESS;
 
 }
 
-Move
-PinkyController::getMove(const GameState& game){
-	return PASS;	
 
-	
+Status PinkyScatter::update(){
+    target=std::make_pair(0,0);
+    if(target.first == -1){
+		target = PinkyInfo::getInfo()->in_gamestate->getMaze().getPowerPillPositions()[0];
+	}
+    auto character = PinkyInfo::getInfo()->in_character;
+    auto gs = PinkyInfo::getInfo()->in_gamestate;
+   
+
+    Move minMove=PASS;
+    std::vector<Move> mover;
+    if(character->getDirection()==PASS) {
+		mover=gs->getMaze().getPossibleMoves(character->getPos());
+	} else {
+		mover = gs->getMaze().getGhostLegalMoves(character->getPos(), character->getDirection());
+	}
+    float min=100000000;
+	for(auto move:mover) {
+		if(move==PASS) {
+			break;
+		}
+		float dist = euclid2(target,gs->getMaze().getNodePos(gs->getMaze().getNeighbour(character->getPos(),move)));
+		if(dist<min) {
+			min=dist;
+			minMove=move;
+		}
+	}
+	PinkyInfo::getInfo()->out_move = minMove;
+	return BH_SUCCESS;
+    
+}
+
+Status PinkyFrihtend::update(){
+	//std::cerr << " Frightened \n" ;
+	auto character = PinkyInfo::getInfo()->in_character;
+	auto gs = PinkyInfo::getInfo()->in_gamestate;
+	std::vector<Move> mover;
+	if(character->getDirection()==PASS) {
+		mover=gs->getMaze().getPossibleMoves(character->getPos());
+	} else {
+		mover = gs->getMaze().getGhostLegalMoves(character->getPos(), character->getDirection());
+	}
+	Move m = mover[rand()%mover.size()];
+	PinkyInfo::getInfo()->out_move = m;
+	return BH_SUCCESS; 
 }
--- src/PinkyController.h
+++ src/PinkyController.h
@@ -1,13 +1,88 @@
 #pragma once
 
 #include "Controller.h"
+#include "BehaviorTree.h"
+#include "BTGhostController.h"
+
+#include <memory>
 
 
-class PinkyController: public Controller {
+class PinkyInfo
+{
+private:
+
+    static PinkyInfo* info;
+
+    PinkyInfo()
+    {
+    }
 
 public:
-	PinkyController(std::shared_ptr<Character> character);
-	virtual ~PinkyController();
-	virtual Move getMove(const GameState& game)override;
+
+    static PinkyInfo* getInfo()
+    {
+        if (info == nullptr)
+        {
+            info = new PinkyInfo();
+        }
+
+        return info;
+    }
+
+    const GameState* in_gamestate;
+
+    Move out_move;
+
+    std::shared_ptr<Character> in_character;
 };
 
+
+class PinkyController : public Controller
+{
+private:
+
+    std::shared_ptr<Composite> root;
+
+public:
+
+    PinkyController(
+        std::shared_ptr<Character> character
+    );
+
+    virtual ~PinkyController();
+
+    virtual Move getMove(
+        const GameState& game
+    ) override;
+};
+
+
+// ============================================================
+// Comportamiento de Pinky
+// ============================================================
+
+class PinkyChase : public Behavior
+{
+public:
+
+    virtual Status update() override;
+};
+class PinkyScatter : public Behavior
+{
+    private:
+        std::pair<int,int> target;
+
+    public:
+        PinkyScatter(){}
+        virtual Status update() override;
+       
+};
+class PinkyFrihtend: public Behavior{
+    private:
+    std::mt19937 e;
+    std::uniform_int_distribution<int> uniform_dist;
+    public:
+        PinkyFrihtend (){}
+        virtual Status update() override;
+        
+};
